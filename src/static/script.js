2bb1590d87f88ecd8b17f8d78781924095d74c6a
Sebastian Sierra <juansebastiansierrac@gmail.com>
2020-10-15 10:58:47 -0500

    fix bugs

--- src/static/script.js
+++ src/static/script.js
@@ -1,27 +1,27 @@
-const validateForm = () => {
+const validateRegister = () => {
     let email = document.forms['registerForm']['email'].value
     let password = document.forms['registerForm']['password'].value
     let c_password = document.forms['registerForm']['c_password'].value
+    let p = document.getElementById('alert')
     correctEmail = validateEmail(email)
     correctPass = validatePassword(password, c_password)
 
     if (!correctEmail) {
-        alert_p = document.getElementById('alert_register')
-        alert_p.innerHTMl = 'Invalid Email'
+        p.innerHTML = 'Invalid email'
         return false
     }
+
     if (!correctPass) {
-        alert_p = document.getElementById('alert_register')
-        alert_p.innerHTMl = "Passwords don't match"
+        p.innerHTML = "Passwords don't match"
         return false
     }
 
     alert_p = document.getElementById('alert_register')
-    alert_p.innerHTMl = 'Registrado'
+    alert_p.innerHTMl = 'Account created'
 }
 
 const validateEmail = (email) => {
-    reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/
+    reg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
     return reg.test(email)
 }
 
